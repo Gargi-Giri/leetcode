b0b20fd502b4345f402355a168057dca9e200b1b
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2024-08-31 18:40:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.7 MB (100.00%)

---
+++ 1894-merge-strings-alternately/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n1=word1.size();
+        int n2=word2.size();
+        int i=0;
+        string s="";
+        while(i<n1||i<n2){
+            if(i<n1){
+                s+=word1[i];
+            }
+            if(i<n2){
+                s+=word2[i];
+            }
+            i++;
+        }
+        return s;
+    }
+};
