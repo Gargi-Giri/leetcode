659ecff59bf15612117d2be390fef0d1e4146ede
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2025-03-03 20:35:09 +0000

    Sync LeetCode submission Runtime - 7 ms (58.80%), Memory - 143.8 MB (7.32%)

---
+++ 2265-partition-array-according-to-given-pivot/solution.cpp
@@ -0,0 +1,84 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot){
+        vector<int> out1;
+        vector<int> out2;
+        vector<int> out3;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<pivot){
+                out1.push_back(nums[i]);
+            }
+            else if(nums[i]==pivot){
+                out2.push_back(nums[i]);
+            }
+            else{
+                out3.push_back(nums[i]);
+            }
+        }
+        vector<int> output;
+        int n1=out1.size(),n2=out2.size(),n3=out3.size();
+        for(int i=0;i<n1;i++){
+            output.push_back(out1[i]);
+        }
+        for(int i=0;i<n2;i++){
+            output.push_back(out2[i]);
+        }
+        for(int i=0;i<n3;i++){
+            output.push_back(out3[i]);
+        }
+        return output;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //O[n] time and space complexity
+//     vector<int> pivotArray(vector<int>& nums, int pivot) {
+//         vector<int> out1;
+//         vector<int> out2;
+//         vector<int> outS;
+//         vector<int> output;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]<pivot){
+//                 out1.push_back(nums[i]);
+//             }
+//             else if(nums[i]>pivot){
+//                 out2.push_back(nums[i]);
+//             }
+//             else{
+//                 outS.push_back(nums[i]);
+//             }
+//         }
+//         int n1=out1.size();
+//         int n2=out2.size();
+//         int n3=outS.size();
+//         for(int i=0;i<n1;i++){
+//             output.push_back(out1[i]);
+//         }
+//         for(int i=0;i<n3;i++){
+//             output.push_back(outS[i]);
+//         }
+//         //output.push_back(pivot);
+//         for(int i=0;i<n2;i++){
+//             output.push_back(out2[i]);
+//         }
+
+//         return output;
+
