14d4484fc77e3880dfcbe3131c66db607b37dcb6
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2025-03-18 15:07:19 +0000

    Sync LeetCode submission Runtime - 3 ms (36.96%), Memory - 61 MB (27.00%)

---
+++ 2478-longest-nice-subarray/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int left = 0, usedBits = 0, maxLen = 0;
+        
+        for (int right = 0; right < nums.size(); ++right) {
+            while ((usedBits & nums[right]) != 0) {
+                usedBits ^= nums[left];  // Remove nums[left] from usedBits
+                left++;
+            }
+            usedBits |= nums[right];  // Add nums[right] to usedBits
+            maxLen = max(maxLen, right - left + 1);
+        }
+        
+        return maxLen;
+    }
+};
+
