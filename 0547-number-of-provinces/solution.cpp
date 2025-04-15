1d3d9d6acf13e1b49c6429786be8e51d9f54e709
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2025-04-15 15:45:32 +0000

    Sync LeetCode submission Runtime - 3 ms (36.65%), Memory - 20.8 MB (25.57%)

--- 0547-number-of-provinces/solution.cpp
+++ 0547-number-of-provinces/solution.cpp
@@ -1,34 +1,37 @@
 class Solution {
 private:
-    void dfs(int node,vector<int> adjLs[],vector<int>& vis){
-        vis[node]=1;
-        for(auto it: adjLs[node]){
-            if(!vis[it]){
-                dfs(it,adjLs,vis);
+    void dfs(int node, vector<vector<int>>& adjLs, vector<bool>& vis) {
+        vis[node] = true;
+        for (int neighbor : adjLs[node]) {
+            if (!vis[neighbor]) {
+                dfs(neighbor, adjLs, vis);
             }
         }
     }
+
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        vector<int> adjLs[n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1&&i!=j){
+        int V = isConnected.size();
+        vector<vector<int>> adjLs(V);  
+
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
                     adjLs[i].push_back(j);
                     adjLs[j].push_back(i);
                 }
             }
         }
-        vector<int> vis(n,0);
-        // int vis[n]={0};
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
+
+        vector<bool> vis(V, false);  // ✅ No C-style array
+        int count = 0;
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
                 count++;
-                dfs(i,adjLs,vis);
+                dfs(i, adjLs, vis);
             }
         }
         return count;
     }
 };
+
