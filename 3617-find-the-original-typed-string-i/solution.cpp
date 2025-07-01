8ba8a5f5bc0b598773e91f8672fc0b77f9c174fd
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2025-07-01 20:25:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.9 MB (62.11%)

---
+++ 3617-find-the-original-typed-string-i/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int count=1;
+        int n=word.size();
+        int i=0;
+        while(i<n){
+            int j=i;
+            while(j<n&&word[j]==word[i]){
+                j++;
+            }
+            int len=j-i;
+            if(len>1){
+                count+=(len-1);
+            }
+            i=j;
+        }
+        return count;
+    }
+};
