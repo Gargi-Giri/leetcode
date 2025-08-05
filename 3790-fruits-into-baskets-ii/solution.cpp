e834a1caac5f514499c9bfe02b70a3b58bb02ace
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2025-08-05 18:58:33 +0000

    Sync LeetCode submission Runtime - 3 ms (49.80%), Memory - 49 MB (31.65%)

--- 3790-fruits-into-baskets-ii/solution.cpp
+++ 3790-fruits-into-baskets-ii/solution.cpp
@@ -3,27 +3,20 @@ public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int n = fruits.size();
         vector<bool> basketUsed(n, false);  
-        int unplacedCount = 0;
-        
+        int unplacedCount = 0;        
         for (int i = 0; i < n; i++) {
-            bool placed = false;
-            
-           
+            bool placed = false;           
             for (int j = 0; j < n; j++) {
-                if (!basketUsed[j] && baskets[j] >= fruits[i]) {
-                   
+                if (!basketUsed[j] && baskets[j] >= fruits[i]) {                   
                     basketUsed[j] = true;
                     placed = true;
                     break;
                 }
-            }
-            
-            
+            }            
             if (!placed) {
                 unplacedCount++;
             }
-        }
-        
+        }        
         return unplacedCount;
     }
 };
