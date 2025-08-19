ba0e215f25448555f3f83adb66add090b2fff4f7
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2025-08-19 12:58:46 +0000

    Sync LeetCode submission Runtime - 1 ms (50.79%), Memory - 111.3 MB (74.80%)

---
+++ 2432-number-of-zero-filled-subarrays/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long count=0, streak=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                streak++;
+                count+=streak;
+            }
+            else{
+                streak=0;
+            }
+        }
+        return count;
+    }
+};
