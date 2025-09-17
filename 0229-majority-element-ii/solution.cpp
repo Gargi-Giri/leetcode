d46220c2ab83de5fc521ee37e3fe890bd9260283
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2025-09-17 18:49:24 +0000

    Sync LeetCode submission Runtime - 7 ms (40.20%), Memory - 26.5 MB (16.71%)

---
+++ 0229-majority-element-ii/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int, int> m;
+        vector<int> out;
+        for(int i=0;i<n;i++){
+            m[nums[i]]++;
+        }
+        n=n/3;
+        for(auto x : m){
+            if(x.second>n){
+                out.push_back(x.first);
+            }
+        }
+        return out;
+    }
+};
