02454886cd1bf82c953625d0879ad6631129360d
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2025-12-23 17:26:53 +0000

    Sync LeetCode submission Runtime - 197 ms (38.82%), Memory - 196.3 MB (58.97%)

--- 2164-two-best-non-overlapping-events/solution.cpp
+++ 2164-two-best-non-overlapping-events/solution.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        int n = events.size();
-        
-        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
-        
-        vector<int> suffixMax(n);
-        suffixMax[n - 1] = events[n - 1][2];
-        
-        for (int i = n - 2; i >= 0; --i) {
-            suffixMax[i] = max(events[i][2], suffixMax[i + 1]);
-        }
-        
-        int maxSum = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            int left = i + 1, right = n - 1;
-            int nextEventIndex = -1;
-            
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (events[mid][0] > events[i][1]) {
-                    nextEventIndex = mid;
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+        sort(events.begin(), events.end());
+        vector<vector<int>> dp(events.size(), vector<int>(3, -1));
+        return findEvents(events, 0, 0, dp);
+    }
+    // Recursive function to find the greatest sum for the pairs.
+    int findEvents(vector<vector<int>>& events, int idx, int cnt,
+                   vector<vector<int>>& dp) {
+        if (cnt == 2 || idx >= events.size()) return 0;
+        if (dp[idx][cnt] == -1) {
+            int end = events[idx][1];
+            int lo = idx + 1, hi = events.size() - 1;
+            while (lo < hi) {
+                int mid = lo + ((hi - lo) >> 1);
+                if (events[mid][0] > end)
+                    hi = mid;
+                else
+                    lo = mid + 1;
             }
-            
-            if (nextEventIndex != -1) {
-                maxSum = max(maxSum, events[i][2] + suffixMax[nextEventIndex]);
-            }
-            
-            maxSum = max(maxSum, events[i][2]);
+            int include =
+                events[idx][2] + (lo < events.size() && events[lo][0] > end
+                                      ? findEvents(events, lo, cnt + 1, dp)
+                                      : 0);
+            int exclude = findEvents(events, idx + 1, cnt, dp);
+            dp[idx][cnt] = max(include, exclude);
         }
-        
-        return maxSum;
+        return dp[idx][cnt];
     }
 };
