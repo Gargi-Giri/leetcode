54fd5c313d786833661ecee042f73d602ea7f892
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2026-01-23 18:20:00 +0000

    Sync LeetCode submission Runtime - 894 ms (32.14%), Memory - 314.7 MB (29.59%)

--- 3772-minimum-pair-removal-to-sort-array-ii/solution.cpp
+++ 3772-minimum-pair-removal-to-sort-array-ii/solution.cpp
@@ -1,132 +1,73 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-struct Node {
-    long long val;
-    int pos;
-    Node* prev;
-    Node* next;
-    bool alive;
-};
- 
-struct Candidate {
-    long long sum;
-    int pos;
-    Node* left;
-    Node* right;
-};
- 
-struct CandComp {
-    bool operator()(const Candidate &a, const Candidate &b) const {
-        if(a.sum == b.sum)
-            return a.pos > b.pos;
-        return a.sum > b.sum;
-    }
-};
- 
-bool isNonDecreasing(Node* head) {
-    Node* cur = head;
-    while(cur && cur->next) {
-        if(cur->val > cur->next->val) return false;
-        cur = cur->next;
-    }
-    return true;
-}
- 
-int minimumPairRemoval(vector<int>& nums) {
-    int n = nums.size();
-    vector<Node*> nodes(n);
-    Node* head = nullptr;
-    Node* prev = nullptr;
-    for (int i = 0; i < n; i++){
-        Node* node = new Node();
-        node->val = nums[i];
-        node->pos = i;
-        node->prev = prev;
-        node->next = nullptr;
-        node->alive = true;
-        if(prev) prev->next = node;
-        else head = node;
-        prev = node;
-        nodes[i] = node;
-    }
-    int violations = 0;
-    Node* cur = head;
-    while(cur && cur->next){
-        if(cur->val > cur->next->val) violations++;
-        cur = cur->next;
-    }
-    if(violations == 0) return 0;
-    priority_queue<Candidate, vector<Candidate>, CandComp> pq;
-    cur = head;
-    while(cur && cur->next){
-        Candidate candid;
-        candid.sum = cur->val + cur->next->val;
-        candid.pos = cur->pos;
-        candid.left = cur;
-        candid.right = cur->next;
-        pq.push(candid);
-        cur = cur->next;
-    }
-    int ops = 0;
-    while(violations > 0 && !pq.empty()){
-        Candidate candid = pq.top();
-        pq.pop();
-        if(!candid.left->alive || !candid.right->alive) continue;
-        if(candid.left->next != candid.right) continue;
-        Node* L = candid.left->prev;
-        Node* R = candid.right->next;
-        if(candid.left->val > candid.right->val) {
-            violations--;
-        }
-        if(L && L->alive){
-            if(L->val > candid.left->val) violations--;
-        }
-        if(R && R->alive){
-            if(candid.right->val > R->val) violations--;
-        }
-        Node* newNode = new Node();
-        newNode->val = candid.left->val + candid.right->val;
-        newNode->pos = candid.left->pos;
-        newNode->prev = L;
-        newNode->next = R;
-        newNode->alive = true;
-        if(L && L->alive){
-            L->next = newNode;
-            if(L->val > newNode->val) violations++;
-        }
-        if(R && R->alive){
-            R->prev = newNode;
-            if(newNode->val > R->val) violations++;
-        }
-        candid.left->alive = false;
-        candid.right->alive = false;
-        if(L){
-            Candidate nc;
-            nc.sum = L->val + newNode->val;
-            nc.pos = L->pos;
-            nc.left = L;
-            nc.right = newNode;
-            pq.push(nc);
-        }
-        if(R){
-            Candidate nc;
-            nc.sum = newNode->val + R->val;
-            nc.pos = newNode->pos;
-            nc.left = newNode;
-            nc.right = R;
-            pq.push(nc);
-        }
-        ops++;
-    }
-    return ops;
-}
- 
+using ll = long long;
+
 class Solution {
 public:
     int minimumPairRemoval(vector<int>& nums) {
-        vector<int> remove = nums;
-        return ::minimumPairRemoval(nums);
+        int n = nums.size();
+        vector<ll> a(n);
+        for (int i = 0; i < n; i++) a[i] = nums[i];
+        
+        // maintain adjacent pairs {sum, index}
+        set<pair<ll, int>> s;
+
+        // double-linked list
+        vector<int> nxt(n);
+        vector<int> pre(n);
+        for (int i = 0; i < n; i++) nxt[i] = i + 1;
+        for (int i = 0; i < n; i++) pre[i] = i - 1;
+
+        // insert all pairs into set
+        int cnt = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i] > a[i + 1]) cnt++;
+            s.insert({a[i] + a[i + 1], i});
+        }
+        
+        // simulate the process
+        int ans = 0;
+        while (cnt > 0) {
+            int i = s.begin()->second;
+            int j = nxt[i];
+            int p = pre[i];
+            int q = nxt[j];
+
+            // pair {i, j}
+            if (a[i] > a[j]) cnt--;
+            if (p >= 0) {
+                // pair {p, i}
+                if (a[p] > a[i] && a[p] <= a[i] + a[j]) {
+                    cnt--;
+                }
+                else if (a[p] <= a[i] && a[p] > a[i] + a[j]) {
+                    cnt++;
+                }
+            }
+            if (q < n) {
+                // pair {j, q}
+                if (a[q] >= a[j] && a[q] < a[i] + a[j]) {
+                    cnt++;
+                }
+                else if (a[q] < a[j] && a[q] >= a[i] + a[j]) {
+                    cnt--;
+                }
+            }
+
+            // remove outdated pairs & add new pairs
+            s.erase(s.begin());
+            if (p >= 0) {
+                s.erase({a[p] + a[i], p});
+                s.insert({a[p] + a[i] + a[j], p});
+            }
+            if (q < n) {
+                s.erase({a[j] + a[q], j});
+                s.insert({a[i] + a[j] + a[q], i});
+                pre[q] = i;
+            }
+            nxt[i] = q;
+            a[i] = a[i] + a[j];
+            ans++;
+        }
+        
+        return ans;
     }
 };
-
