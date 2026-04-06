dac45831b9864a044b50f0cd297094f75b089e32
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2026-04-06 18:05:58 +0000

    Sync LeetCode submission Runtime - 10 ms (98.86%), Memory - 34.4 MB (98.09%)

--- 0906-walking-robot-simulation/solution.cpp
+++ 0906-walking-robot-simulation/solution.cpp
@@ -1,36 +1,36 @@
+#include <memory_resource>
+pmr::unsynchronized_pool_resource pool;
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        vector<pair<int,int>> directions={{0,1},{1,0},{0,-1},{-1,0}};
-        unordered_set<string> obstacleSet;
-        for (const auto& obstacle : obstacles) {
-        obstacleSet.insert(to_string(obstacle[0]) + "," + to_string(obstacle[1]));
+        const unsigned lb=30000;
+        pmr::unordered_set<unsigned> obSet(&pool);
+        obSet.reserve(obstacles.size());
+        for(auto& ob: obstacles){
+            const unsigned x=ob[0]+lb, y=ob[1]+lb;
+            obSet.insert((x<<16)+y);
         }
-    
-        int x=0,y=0;
-        int directionIndex=0;
-        int maxDistance=0;
-        for(int command:commands){
-            if(command==-2){
-                directionIndex=(directionIndex+3)%4;
-            }
-            else if(command==-1){
-                directionIndex=(directionIndex+1)%4;
-            }
-            for(int i=0;i<command;++i){
-                int newX=x+directions[directionIndex].first;
-                int newY=y+directions[directionIndex].second;
-                if (obstacleSet.find(to_string(newX) + "," + to_string(newY)) != obstacleSet.end()) {
-                    break; // Stop moving if we hit an obstacle
-                }
-                
-                x = newX;
-                y = newY;
-                
-                // Calculate the squared distance and update max distance
-                maxDistance = max(maxDistance, x * x + y * y);
+
+        const int dir[4][2]={{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+        int x=0, y=0, dx=0, dy=1, face=0, maxD2=0;
+        for(int c: commands){
+            switch(c){
+                case -2: face=(face+1)&3; dx=dir[face][0]; dy=dir[face][1]; break;
+                case -1: face=(face+3)&3; dx=dir[face][0]; dy=dir[face][1]; break;
+                default:
+                //    cout<<"\nc="<<c<<" face="<<face<<":";
+                    for(int i=0; i<c; i++){
+                        x+=dx, y+=dy;
+                        if (obSet.count(((x+lb)<<16)+y+lb)) {
+                            x-=dx;  // previous move
+                            y-=dy;
+                            break;
+                        }
+                    //    cout<<"("<<x<<","<<y<<"),";
+                        maxD2=max(maxD2, x*x+y*y);
+                    }
             }
         }
-            return maxDistance;
+        return maxD2;
     }
 };
