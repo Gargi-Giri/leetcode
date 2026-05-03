291ca9808719e00bb35632cfe41fdcba757bdb45
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2026-05-03 10:35:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8 MB (90.68%)

--- 0812-rotate-string/solution.cpp
+++ 0812-rotate-string/solution.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if (s.length() != goal.length()) {
-            return false;
+        if (s.length() != goal.length()) return false;
+        int length = s.length();
+
+        // Try all possible rotations of the string
+        for (int rotationCount = 0; rotationCount < length; ++rotationCount) {
+            // Perform one rotation
+            rotate(s.begin(), s.begin() + 1, s.end());
+            if (s == goal) return true;
         }
-        return (s + s).find(goal) != string::npos;
+        return false;
     }
 };
