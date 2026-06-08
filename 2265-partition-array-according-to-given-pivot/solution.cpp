172320dec2a964bacc6294d390239844f8e09dd7
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2026-06-08 18:40:55 +0000

    Sync LeetCode submission Runtime - 261 ms (5.39%), Memory - 233.2 MB (7.32%)

--- 2265-partition-array-according-to-given-pivot/solution.cpp
+++ 2265-partition-array-according-to-given-pivot/solution.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> out1;
-        vector<int> out2;
-        vector<int> outS;
-        vector<int> output;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<pivot){
-                out1.push_back(nums[i]);
+        list<int> less;
+        list<int> equal;
+        list<int> greater;
+
+        for (auto num : nums) {
+            if (num < pivot) {
+                less.push_back(num);
+            } else if (num > pivot) {
+                greater.push_back(num);
+            } else {
+                equal.push_back(num);
             }
-            else if(nums[i]>pivot){
-                out2.push_back(nums[i]);
-            }
-            else{
-                outS.push_back(nums[i]);
-            }
-        }
-        int n1=out1.size();
-        int n2=out2.size();
-        int n3=outS.size();
-        for(int i=0;i<n1;i++){
-            output.push_back(out1[i]);
-        }
-        for(int i=0;i<n3;i++){
-            output.push_back(outS[i]);
-        }
-        //output.push_back(pivot);
-        for(int i=0;i<n2;i++){
-            output.push_back(out2[i]);
         }
 
-        return output;
+        less.insert(less.end(), equal.begin(), equal.end());
+        less.insert(less.end(), greater.begin(), greater.end());
+
+        vector<int> ans;
+        for (auto num : less) {
+            ans.push_back(num);
+        }
+        return ans;
     }
 };
