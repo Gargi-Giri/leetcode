d65400425388d893c9ab0aafd9b5676ab10ce049
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2026-06-15 17:41:05 +0000

    Sync LeetCode submission Runtime - 4 ms (35.81%), Memory - 312 MB (54.06%)

--- 2216-delete-the-middle-node-of-a-linked-list/solution.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/solution.cpp
@@ -1,53 +1,22 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-// Definition for singly-linked list.
-// struct ListNode {
-//     int val;
-//     ListNode *next;
-//     ListNode(int x) : val(x), next(nullptr) {}
-// };
-
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        // If the list has only one node, return nullptr (since we are deleting it)
-        if (!head || !head->next) return nullptr;
-        
-        // Initialize slow and fast pointers, and a previous pointer
-        ListNode *slow = head, *fast = head, *prev = nullptr;
-        
-        // Move the slow pointer by 1 step and fast pointer by 2 steps
+        if (!head->next) return nullptr;
+
+        ListNode* slow = head;
+        ListNode* fast = slow->next->next;
+
         while (fast && fast->next) {
-            prev = slow;
             slow = slow->next;
             fast = fast->next->next;
         }
-        
-        // Now 'slow' is pointing to the middle node, and 'prev' is the node before it
-        // Remove the middle node by skipping it in the linked list
-        prev->next = slow->next;
-        
-        // Return the modified list
+
+        slow->next = slow->next->next;
         return head;
     }
 };
 
-// Utility function to print the linked list
-void printList(ListNode* head) {
-    ListNode* temp = head;
-    while (temp != nullptr) {
-        cout << temp->val << " ";
-        temp = temp->next;
-    }
-    cout << endl;
-}
 
 
+// Memory Deallocation
+// LeetCode environment automatically frees the bypassed middle node
