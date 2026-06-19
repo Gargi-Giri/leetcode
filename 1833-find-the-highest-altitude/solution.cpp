697005924eb19c34ac91021237ff33ae76deb3c6
Gargi Giri <141861093+Gargi-Giri@users.noreply.github.com>
2026-06-19 18:07:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11 MB (14.96%)

--- 1833-find-the-highest-altitude/solution.cpp
+++ 1833-find-the-highest-altitude/solution.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int currAlt=0,maxAlt=0;
-        for(int i=0;i<gain.size();++i){
-            currAlt+=gain[i];
-            maxAlt=max(maxAlt,currAlt);
+        int currentAltitude = 0;
+        // Highest altitude currently is 0.
+        int highestPoint = currentAltitude;
+        
+        for (int altitudeGain : gain) {
+            // Adding the gain in altitude to the current altitude.
+            currentAltitude += altitudeGain;
+            // Update the highest altitude.
+            highestPoint = max(highestPoint, currentAltitude);
         }
-        return maxAlt;
+        
+        return highestPoint;
     }
 };
